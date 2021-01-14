6237b1c0e6bd334a3aa5e42cba3443eab6a08c16
Roman <nopbreak@gmail.com>
2021-01-14 09:24:58 +0300

    core: refactoring packages

--- Hooks/src/main/java/tv/twitch/android/mod/hooks/Jump.java
+++ Hooks/src/main/java/tv/twitch/android/mod/hooks/Jump.java
@@ -1,7 +1,7 @@
 package tv.twitch.android.mod.hooks;
 
 
-import tv.twitch.android.mod.settings.PreferenceManager;
+import tv.twitch.android.mod.setting.PreferenceManager;
 
 
 public final class Jump {
