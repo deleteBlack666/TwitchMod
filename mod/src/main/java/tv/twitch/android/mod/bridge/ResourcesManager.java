381026916649e07ac5154a30dee367a5043dae6f
Roman <nopbreak@gmail.com>
2021-01-17 09:27:31 +0300

    mod: remove STYLEABLES from ResourcesManager

--- mod/src/main/java/tv/twitch/android/mod/bridge/ResourcesManager.java
+++ mod/src/main/java/tv/twitch/android/mod/bridge/ResourcesManager.java
@@ -14,9 +14,6 @@ import tv.twitch.android.mod.util.Logger;
 public class ResourcesManager {
     private static final HashMap<IdType, PubCache> CACHE = new HashMap<>();
 
-    private static final HashMap<String, int[]> STYLEABLES = new HashMap<>();
-
-
     private enum IdType {
         STRING("string"),
         ID("id"),
@@ -44,63 +41,6 @@ public class ResourcesManager {
         for (IdType idType : IdType.values()) {
             CACHE.put(idType, new PubCache(200, idType.getType()));
         }
-
-        STYLEABLES.put("CheckBoxPreference", new int[] {android.R.attr.summaryOn,
-                android.R.attr.summaryOff, android.R.attr.disableDependentsState,
-                getAttrId("disableDependentsState"), getAttrId("summaryOff"),
-                getAttrId("summaryOn")});
-        STYLEABLES.put("DialogPreference", new int[] {android.R.attr.dialogTitle,
-                android.R.attr.dialogMessage, android.R.attr.dialogIcon,
-                android.R.attr.positiveButtonText, android.R.attr.negativeButtonText,
-                android.R.attr.dialogLayout, getAttrId("dialogIcon"),
-                getAttrId("dialogLayout"), getAttrId("dialogMessage"),
-                getAttrId("dialogTitle"), getAttrId("negativeButtonText"),
-                getAttrId("positiveButtonText")});
-        STYLEABLES.put("EditTextPreference", new int[] {getAttrId("useSimpleSummaryProvider")});
-        STYLEABLES.put("PreferenceImageView", new int[] {android.R.attr.maxWidth,
-                android.R.attr.maxHeight, getAttrId("maxHeight"), getAttrId("maxWidth")});
-        STYLEABLES.put("ListPreference", new int[] {android.R.attr.entries, android.R.attr.entryValues,
-                getAttrId("entries"), getAttrId("entryValues"),
-                getAttrId("useSimpleSummaryProvider")});
-        STYLEABLES.put("Preference", new int[] {android.R.attr.icon, android.R.attr.persistent,
-                android.R.attr.enabled, android.R.attr.layout,
-                android.R.attr.title, android.R.attr.selectable, android.R.attr.key,
-                android.R.attr.summary, android.R.attr.order, android.R.attr.widgetLayout,
-                android.R.attr.dependency, android.R.attr.defaultValue,
-                android.R.attr.shouldDisableView, android.R.attr.fragment,
-                getAttrId("allowDividerAbove"), getAttrId("allowDividerBelow"),
-                getAttrId("defaultValue"), getAttrId("dependency"),
-                getAttrId("enableCopying"), getAttrId("enabled"),
-                getAttrId("fragment"), getAttrId("icon"), getAttrId("iconSpaceReserved"),
-                getAttrId("isPreferenceVisible"), getAttrId("key"), getAttrId("layout"),
-                getAttrId("order"), getAttrId("persistent"), getAttrId("selectable"),
-                getAttrId("shouldDisableView"), getAttrId("singleLineTitle"),
-                getAttrId("summary"), getAttrId("title"), getAttrId("widgetLayout")});
-        STYLEABLES.put("MultiSelectListPreference", new int[] {android.R.attr.entries,
-                android.R.attr.entryValues, getAttrId("entries"), getAttrId("entryValues")});
-        STYLEABLES.put("PreferenceFragment", new int[] {android.R.attr.layout, android.R.attr.divider,
-                android.R.attr.dividerHeight, getAttrId("allowDividerAfterLastItem")});
-        STYLEABLES.put("PreferenceFragmentCompat", new int[] {android.R.attr.layout, android.R.attr.divider,
-                android.R.attr.dividerHeight, getAttrId("allowDividerAfterLastItem")});
-        STYLEABLES.put("PreferenceGroup", new int[] {android.R.attr.orderingFromXml,
-                getAttrId("initialExpandedChildrenCount"), getAttrId("orderingFromXml")});
-        STYLEABLES.put("BackgroundStyle", new int[] {android.R.attr.selectableItemBackground,
-                getAttrId("selectableItemBackground")});
-        STYLEABLES.put("SeekBarPreference", new int[] {android.R.attr.layout, android.R.attr.max,
-                getAttrId("adjustable"), getAttrId("min"),
-                getAttrId("seekBarIncrement"), getAttrId("seekBarValueSuffix"),
-                getAttrId("showSeekBarValue"), getAttrId("updatesContinuously")});
-        STYLEABLES.put("SwitchPreferenceX", new int[] {android.R.attr.summaryOn, android.R.attr.summaryOff,
-                android.R.attr.disableDependentsState, android.R.attr.switchTextOn,
-                android.R.attr.switchTextOff, getAttrId("disableDependentsState"),
-                getAttrId("summaryOff"), getAttrId("summaryOn"), getAttrId("switchTextOff"),
-                getAttrId("switchTextOn")});
-        STYLEABLES.put("SwitchPreferenceCompatX", new int[] {android.R.attr.summaryOn,
-                android.R.attr.summaryOff, android.R.attr.disableDependentsState,  android.R.attr.switchTextOn,
-                android.R.attr.switchTextOff, getAttrId("disableDependentsState"),
-                getAttrId("summaryOff"), getAttrId("summaryOn"),
-                getAttrId("switchTextOff"), getAttrId("switchTextOn")
-        });
     }
 
     private static class PubCache extends LruCache<String, Integer> {
@@ -211,14 +151,5 @@ public class ResourcesManager {
         } else {
             return getStringById(resId);
         }
-    }
-
-    public static int[] getStyleableArr(String name) {
-        if (TextUtils.isEmpty(name)) {
-            Logger.error("empty name");
-            return new int[] {};
-        }
-
-        return STYLEABLES.get(name);
     }
 }
